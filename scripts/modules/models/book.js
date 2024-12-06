15c6c07cbb64cd146a1a4eb7aa34c14f073b0bc8
azanra <112457939+azanra@users.noreply.github.com>
2024-12-06 11:15:52 +0700

    Remove unused getter and setter in books class

--- scripts/modules/models/book.js
+++ scripts/modules/models/book.js
@@ -6,46 +6,6 @@ class Book {
         this.genre = genre;
         this.synopsis = synopsis;
     }
-
-    get id() {
-        return this.id;
-    }
-
-    set id(newId) {
-        this.id = newId;
-    }
-
-    get title() {
-        return this.title;
-    }
-
-    set title(newTitle) {
-        this.title = newTitle
-    }
-
-    get author() {
-        return this.author;
-    }
-
-    set author(newAuthor) {
-        this.author = newAuthor;
-    }
-
-    get genre() {
-        return this.genre;
-    }
-
-    set genre(newGenre) {
-        this.genre = newGenre;
-    }
-
-    get synopsis(){
-        return this.synopsis;
-    }
-
-    set synopsis(newSynopsis){
-        this.synopsis = newSynopsis;
-    }
 }
 
 export {Book};
---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+import { Books } from "./modules/models/books.js";
+import { Book } from "./modules/models/book.js";
+
+let bookArr = new Books();
+bookArr.newBook("1", "title", "author", "genre", "synopsis");
+bookArr.newBook("2", "anotherTitle", "anotherAuthor", "anotherGenre", "anotherSynopsis")
+console.log(bookArr);
+
+let anotherArr = new Books();
+anotherArr.newBook("id", "title", "author", "genre", "synopsis");
+console.log(anotherArr)
+anotherArr.updateAttr("id", "three days of happiness", "sugaru miaki", "romance", "-");
+
+anotherArr.newBook("3", "title", "author", "genre", "synopsis");
+console.log(anotherArr)
+anotherArr.deleteBook("3");
+
+
+console.log(anotherArr);
